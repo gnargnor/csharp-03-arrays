a0914256315d79ca503ad487882d3d400799e27f
Logan Kelly <logan.r.kelly@gmail.com>
2017-06-14 20:23:29 -0500

    orders a user defined number of integers

---
+++ csharp-03-arrays/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace csharp03arrays
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("This program will sort numbers for you.");
+
+            Console.WriteLine("How many numbers do you want to sort?");
+
+            int numberOfInputs = Convert.ToInt32(Console.ReadLine());
+
+            int[] allNumbers = new int[numberOfInputs];
+
+            for (int i = 0; i < numberOfInputs; i++){
+                Console.WriteLine("Number " + (i + 1));
+                allNumbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Array.Sort(allNumbers);
+            Console.WriteLine();
+
+            Console.WriteLine("Here ya go, Chief:");
+            for (int j = 0; j < allNumbers.Length; j++){
+                Console.WriteLine((j+1) + ")   " + allNumbers[j]);
+            }
+
+        }
+    }
+}
